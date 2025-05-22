078da0cad885cc76d1fdd0a1da0b83173387407a
tegarmuhamad922@gmail.com <tegarmuhamad922@gmail.com>
2025-05-22 14:11:46 +0700

    modifi button sidebar

--- src/components/admin/sidebar/SidebarMain.vue
+++ src/components/admin/sidebar/SidebarMain.vue
@@ -2,7 +2,7 @@
   <div class="">
     <button @click="toggleSidebar"
       class="fixed top-4 left-4 z-50 p-2 bg-quinary rounded-lg transition-all duration-300  ease-in-out md:hidden"
-      :class="isSidebarOpen ? 'left-24' : 'left-4'">
+      :class="isSidebarOpen ? 'translate-x-24' : 'translate-x-0'">
       <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7" />
       </svg>
